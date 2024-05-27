bf2867e7825d7f6390fce6730ba85dfd273ba635
Tonysoossa <tony.servanin@gmail.com>
2024-05-27 18:27:00 +0200

    login + errors completed

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -15,37 +15,34 @@ const logFunc = () => {
     const txt401 = document.createTextNode(
       "Not Authorized, verify email or password"
     );
-    try {
-      const res = await fetch(`${API_ENDPOINT}/users/login`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
-      if (res.status === 404) {
-        throw new Error(
-          (logErr.style.display = "block"),
-          logErr.appendChild(txt404)
-        );
-      }
-      if (res.status === 401) {
-        throw new Error(
-          (logErrMdp.style.display = "block"),
-          logErrMdp.appendChild(txt401)
-        );
-      }
 
-      const dataLogin = await res.json();
-      console.log(dataLogin);
-      localStorage.setItem("token", dataLogin.token);
-      if (dataLogin.token) {
-        window.location.assign(
-          "http://localhost:5500/Portfolio-architecte-sophie-bluel/FrontEnd/index.html"
-        );
-      }
-    } catch (err) {
-      console.log(err);
+    const res = await fetch(`${API_ENDPOINT}/users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (res.status === 404) {
+      logErr.style.display = "block";
+      logErr.innerHTML = "";
+      logErr.appendChild(txt404);
+    }
+
+    if (res.status === 401) {
+      logErrMdp.style.display = "block";
+      logErrMdp.innerHTML = "";
+      logErrMdp.appendChild(txt401);
+    }
+
+    const dataLogin = await res.json();
+    console.log(dataLogin);
+    localStorage.setItem("token", dataLogin.token);
+    if (dataLogin.token) {
+      window.location.assign(
+        "http://localhost:5500/Portfolio-architecte-sophie-bluel/FrontEnd/index.html"
+      );
     }
   });
 };
